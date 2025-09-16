a8c8d226a0a1049126ffefda49b000290ea5ed76
Calebwoe <143655176+Calebwoe@users.noreply.github.com>
2025-09-15 23:45:55 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,36 @@
 d3.json("GlobalTemperatures.json").then(function(data) {
   console.log(data);  // <- check that data is loading
 });
+
+
+d3.json("GlobalTemperatures.json").then(function(data) {
+  const svg = d3.select("#chart1")
+                .attr("width", 900)
+                .attr("height", 500);
+
+  const width = 900, height = 500;
+
+  // Create scales
+  const xScale = d3.scaleLinear()
+      .domain(d3.extent(data, d => d.time))   // min/max of time
+      .range([40, width-40]);
+
+  const yScale = d3.scaleLinear()
+      .domain(d3.extent(data, d => d.LAT))    // min/max temp
+      .range([height-40, 40]);
+
+  const rScale = d3.scaleLinear()
+      .domain(d3.extent(data, d => d.LATU))   // min/max uncertainty
+      .range([2, 10]);
+
+  // Bind data to circles
+  svg.selectAll("circle")
+    .data(data)
+    .enter()
+    .append("circle")
+    .attr("cx", d => xScale(d.time))
+    .attr("cy", d => yScale(d.LAT))
+    .attr("r", d => rScale(d.LATU))
+    .attr("fill", "steelblue")
+    .attr("opacity", 0.6);
+});
